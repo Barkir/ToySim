2be8aa3dd7c4e950214553338994dc3ef42a82fb
Barkir <r.barinov2006@gmail.com>
2025-10-12 19:27:43 +0300

    Added function for getting file size

---
+++ build/CMakeFiles/ToySim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToySim.dir/link.d"
+  "CMakeFiles/ToySim.dir/src/interpreter.cpp.o"
+  "CMakeFiles/ToySim.dir/src/interpreter.cpp.o.d"
+  "ToySim"
+  "ToySim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToySim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
