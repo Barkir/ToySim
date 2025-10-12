e5683ad825430ca0f1308ad1fe88833986c258b5
Barkir <r.barinov2006@gmail.com>
2025-10-12 19:55:30 +0300

    Added hexdump function

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -2,3 +2,5 @@
 
 enum toyErrors get_commands(std::vector<uint32_t> *commands, const std::string& filename);
 size_t getFileSize(std::ifstream& file);
+
+void hexDump(std::vector<char> buffer);
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -17,27 +17,34 @@ int main(void) {
     }
 }
 
+// -------------------------------------------------------------------------------------------
+
 // returns an array of 32-bit commands
 enum toyErrors get_commands(std::vector<uint32_t> *commands, const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
+
     if (!file) {
         std::cerr << "Error reading file: " << filename << "\n";
         return TOY_FILE_READ_ERROR;
     }
 
     size_t fSize = getFileSize(file);
-    if (fSize % COMMAND_SIZE) {
+    if (fSize % COMMAND_SIZE || fSize == 0) {
         std::cerr << "WRONG FILE SIZE " << fSize << "\n";
         file.close();
         return TOY_WRONG_FILE_SIZE;
     }
 
     if (file.is_open()) {
-        file.close();
-
+        std::vector<char> buffer(fSize);
+        file.read(buffer.data(), fSize);
+        hexDump(buffer);
     }
     return TOY_FAILED;
 }
+
+// -----------------------------------------------------------------------------------------------
+
 size_t getFileSize(std::ifstream& file) {
     if (!file.is_open()) return 0;
 
@@ -46,6 +53,20 @@ size_t getFileSize(std::ifstream& file) {
     file.seekg(0, std::ios::beg);
 
     return size;
+}
+
+void hexDump(std::vector<char> buffer) {
+    fprintf(stdout, RED "===========sup=============\n" RESET);
+    fprintf(stdout, GREEN "▁▂▃▄▅▆▇▉ (.) (.) ▉▇▆▅▄▃▂▁\n" RESET);
+    int cnt = 0;
+    for (char& c: buffer) {
+        if (cnt % 4 == 0) {
+            fprintf(stdout, CYAN "\n 0x%-2X ->" RESET, cnt / 4);
+        }
+        fprintf(stdout, "%4X", c);
+        cnt++;
+    }
+    fprintf(stdout, RED "\n\n===========bye=============\n" RESET);
 }
 
 
