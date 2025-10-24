739a06dd4e97b658202928e85350d231f7734ffd
Barkir <r.barinov2006@gmail.com>
2025-10-24 20:11:50 +0300

    Removed big switch created hash table with functions

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -50,68 +50,9 @@ void init(std::vector<uint32_t> commands, size_t fsize) {
 
         uint32_t opcode = commandObj.getOpcode();
         auto it = OPCODE_MAP.find(opcode);
-        // it->second(spu, commandObj);
-        // ON_DEBUG(spuDump(spu));
-        switch (it->first) {
-            case TOY_XOR:
-                callXOR(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-            case TOY_MOVN:
-                callMOVN(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
+        it->second(spu, commandObj);
+        ON_DEBUG(spuDump(spu));
 
-            case TOY_ADD:
-                callADD(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_SUBI:
-                callSUBI(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_JMP:
-                callJMP(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_BEQ:
-                callBEQ(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_SYSCALL:
-                callSYSCALL(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_LD:
-                callLD(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_LDP:
-                callLDP(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_CLS:
-                callCLS(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_RORI:
-                callRORI(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-
-            case TOY_ST:
-                callST(spu, commandObj);
-                ON_DEBUG(spuDump(spu));
-                break;
-        }
     }
 }
 
